442a8b7f23d9ae67983e987d467d2b50c2f9a0af
Saurabh chawda <saurabhchawda2000@gmail.com>
2022-03-20 18:20:05 +0530

    Feat : Added Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
-import {Home} from '../src/Pages/Home-Page/Home'
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../src/Pages/Home-Page/Home";
+import { Product } from "../src/Pages/ProductPage/Product";
 function App() {
   return (
-    <Home />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products" element={<Product />}></Route>
+      </Routes>
+    </div>
   );
 }
 
