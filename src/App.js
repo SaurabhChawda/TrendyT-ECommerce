96a546971e40b25202828ff507000de3e25dbdba
Saurabh chawda <saurabhchawda2000@gmail.com>
2022-03-23 14:53:30 +0530

    feat : Added Wishlist Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../src/Pages/Home-Page/Home";
 import { Product } from "../src/Pages/ProductPage/Product";
+import { Wishlist } from "../src/Pages/Wishlist-page/Wishlist";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/products" element={<Product />}></Route>
+        <Route path="/wishlist" element={<Wishlist />}></Route>
       </Routes>
     </div>
   );
---
+++ src/Pages/Wishlist-page/Components/WishlistHeader.jsx
@@ -0,0 +1,7 @@
+export const WishlistHeader = () => {
+  return (
+    <section className="wishlist__header">
+      <h3 className="wishlist__title">My Wishlist(0)</h3>
+    </section>
+  );
+};
