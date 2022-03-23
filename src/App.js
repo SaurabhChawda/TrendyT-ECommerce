0481db0de1b45e3b7fcae5eb5734de2769101665
Saurabh chawda <saurabhchawda2000@gmail.com>
2022-03-23 18:39:20 +0530

    feat : Added Cart page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "../src/Pages/Home-Page/Home";
 import { Product } from "../src/Pages/ProductPage/Product";
 import { Wishlist } from "../src/Pages/Wishlist-page/Wishlist";
+import { Cart } from "../src/Pages/Cart-Page/Cart";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/products" element={<Product />}></Route>
-        <Route path="/wishlist" element={<Wishlist />}></Route>
+        <Route path="/products/" element={<Product />}></Route>
+        <Route path="/wishlist/" element={<Wishlist />}></Route>
+        <Route path="/cart/" element={<Cart />}></Route>
       </Routes>
     </div>
   );
---
+++ src/Pages/Cart-Page/Components/CartHeader.jsx
@@ -0,0 +1,7 @@
+export const CartHeader = () => {
+  return (
+    <section className="cart__header">
+      <h3 className="cart__title">My Cart(0)</h3>
+    </section>
+  );
+};
