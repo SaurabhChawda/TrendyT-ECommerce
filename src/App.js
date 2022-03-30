6edb14ebadc7296e9c7e2cfcf194b73d024c3533
Saurabh chawda <saurabhchawda2000@gmail.com>
2022-03-30 09:58:27 +0530

    Feat: Added Static Login Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home } from "../src/Pages/Home-Page/Home";
 import { Product } from "../src/Pages/ProductPage/Product";
 import { Wishlist } from "../src/Pages/Wishlist-page/Wishlist";
 import { Cart } from "../src/Pages/Cart-Page/Cart";
+import { Login } from "../src/Pages/Login-Page/Login";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
         <Route path="/products/" element={<Product />}></Route>
         <Route path="/wishlist/" element={<Wishlist />}></Route>
         <Route path="/cart/" element={<Cart />}></Route>
+        <Route path="/login/" element={<Login />}></Route>
       </Routes>
     </div>
   );
