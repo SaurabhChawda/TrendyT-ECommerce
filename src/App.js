ace10a07bed69635a0ace9f2b3e86b107824c2be
Saurabh chawda <saurabhchawda2000@gmail.com>
2022-05-08 23:27:13 +0530

    fix bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Home, Product, Wishlist, Cart, SignUp, Login } from "../src/Pages/Index";
+import { Home, Product, Wishlist, Cart, Signup, Login } from "../src/Pages/Index";
 import { PrivateRoute } from "./PrivateRoute/PrivateRoute";
 import Mockman from "mockman-js";
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/products/" element={<Product />}></Route>
-        <Route path="/signUp/" element={<SignUp />}></Route>
+        <Route path="/signUp/" element={<Signup />}></Route>
         <Route path="/login/" element={<Login />}></Route>
         <Route
           path="/wishlist/"
