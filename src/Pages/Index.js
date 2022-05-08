417817003ac7400884e35df277b7f4ad0efddd57
Saurabh chawda <saurabhchawda2000@gmail.com>
2022-05-08 23:13:43 +0530

    fix bugs

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -3,4 +3,4 @@ export { Product } from "./ProductPage/Product.jsx";
 export { Cart } from "./Cart-Page/Cart.jsx";
 export { Wishlist } from "./Wishlist-page/Wishlist.jsx";
 export { Login } from "./Login-Page/Login.jsx";
-export { SignUp } from './Signup-Page/SignUp.jsx'
+export { SignUp } from './SignUp-Page/SignUp.jsx'
